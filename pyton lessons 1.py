ffc61f5729b283e49c97872404878a8ad7d5064a
Gabriel Harrison <gabriel.harrison@tooeleschools.org>
2018-09-07 09:19:30 -0600

    unit one examples

---
+++ pyton lessons 1.py
@@ -0,0 +1,14 @@
+#var_number
+var_name= "value"
+message_two="this is a string of data"
+message_one="that we must discuss"
+message_three=message_two +" "+ message_one
+print(message_three)
+num1=2
+num2=1.5
+num3= num1 + num2
+
+#actions
+print(num3)
+print(type(num2))
+print(message_three, num3)
---
+++ unit 1 exe.py
@@ -0,0 +1,21 @@
+print("this is a string")
+name= "gabriel"
+print(type(name))
+fact= "I like blizzard games"
+message= "my name is" +" "+ name +" "+ "and" +" "+ fact
+print(message)
+
+num= 8
+num2= 14
+print(num + num2)
+answer= num-num2
+print(answer)
+
+word1= input("enter an object or creature")
+word2= input("location")
+word3= input("business")
+word4= input("name")
+word5= input("adjective")
+print("A", word1, "went to", word2, "to talk to", "a", word5, "man named", word4, "at", word3 + ".") 
+
+input()
